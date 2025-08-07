997262fdb84694e9cdfbb98511fc4fa285b75490
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-08-07 22:03:38 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
-  plugins: [react()],
+    base: "/paradise-nursery-shopping-cart-app",
+    plugins: [react()],
 })
